8d42cc721ddc0a5fe38d61a8c814fbe8b10ba9ab
bjlhx15 <bjlhx15@163.com>
2018-08-09 22:34:13 +0800

    init

---
+++ discovery-eureka-single-security/src/main/java/com/lhx/springcloud/discovery/DiscoveryEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.lhx.springcloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryEurekaApplication.class, args);
+    }
+}
