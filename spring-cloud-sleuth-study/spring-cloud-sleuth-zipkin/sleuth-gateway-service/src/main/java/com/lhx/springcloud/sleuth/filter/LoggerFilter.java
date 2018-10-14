e622a7b61ce94efa1e53fa2e580014de397dd881
lihongxu6 <lihongxu6@jd.com>
2018-10-14 16:31:38 +0800

    init

---
+++ spring-cloud-sleuth-study/spring-cloud-sleuth-zipkin/sleuth-gateway-service/src/main/java/com/lhx/springcloud/sleuth/filter/LoggerFilter.java
@@ -0,0 +1,27 @@
+package com.lhx.springcloud.sleuth.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author lihongxu
+ * @desc @link(https://github.com/bjlhx15/spring-cloud-base)
+ * @since 2018/10/14 下午4:19
+ */
+public class LoggerFilter implements GlobalFilter,Ordered {
+//    @Autowired
+//    Tracer tracer;
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return Integer.MAX_VALUE;
+    }
+}
---
+++ spring-cloud-sleuth-study/spring-cloud-sleuth-zipkin/sleuth-user-service/src/main/java/com/lhx/springcloud/provider/business/BusinessServiceApplication7901.java
@@ -0,0 +1,11 @@
+package com.lhx.springcloud.provider.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceApplication7901 {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessServiceApplication7901.class, args);
+    }
+}
